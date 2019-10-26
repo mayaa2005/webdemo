cc96b4eaac45b2a0cc6c744ed8546e2c82dca47c
yuan jie <mayaa2005@qq.com>
2019-10-26 22:02:39 +0800

    V3.0 新增CoffeeRun项目

---
+++ front-end-dev-book/coffeerun/scripts/main.js
@@ -0,0 +1,28 @@
+(function(window) {
+  'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var CHECKLIST_SELECTR = '[data-coffee-order="checklist"]';
+
+  var App = window.App;
+  var Truck = App.Truck;
+  var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
+  var Validation = App.Validation;
+  var CheckList = App.CheckList;
+
+  var myTruck = new Truck('ncc-1701', new DataStore());
+  window.myTruck = myTruck;
+  var checkList = new CheckList(CHECKLIST_SELECTR);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+
+  var formHandler = new FormHandler(FORM_SELECTOR);
+
+  formHandler.addSubmitHandler(function(data){
+    myTruck.createOrder.call(myTruck,data);
+    checkList.addRow.call(checkList,data);
+  });
+  console.log(formHandler);
+
+  formHandler.addInputHandler(Validation.isCompanyEmail);
+
+})(window);
